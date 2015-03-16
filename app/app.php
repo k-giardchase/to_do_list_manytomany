71bf4f1cfd2275b389c861e6ad1f55181d678916
geoff winner & james nielson <student@epicodus.com>
2015-03-16 14:27:14 -0700

    get touched

---
+++ app/app.php
@@ -0,0 +1,31 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php;"
+    require_once __DIR__."/../src/CategoryTask.php";
+
+    $app = new Silex\Application();
+
+    $DB = new PDO('pgsql:host=localhost;dbname=to_do');
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+     'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+     return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
+    });
+
+    $app->post("/tasks", function() use ($app) {
+        $task = new Task($_POST['description']);
+        $task->save();
+        return $app['twig']->('create_task.twig', array('newtask' => $task));
+    });
+
+    $app->post("/delete_tasks", function() use ($app) {
+        Task::deleteAll();
+        return $app['twig']->render('delete_tasks.twig');
+    });
+
+    return $app;
+
+
+?>
